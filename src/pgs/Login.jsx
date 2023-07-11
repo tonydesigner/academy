a8db08807e09fff5a0a62263692cb6dd34e5fb15
Antonio Santos <84925495+tonydesigner@users.noreply.github.com>
2023-07-11 17:04:01 -0300

    update acao

--- src/pgs/Login.jsx
+++ src/pgs/Login.jsx
@@ -7,6 +7,7 @@ export function Login() {
     e.preventDefault();
     
     if (login == "admin" && senha == "admin") {
+      $('.login form').css('animation', 'acao 1s')
        $('.login').hide()
     }
   };
