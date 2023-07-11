e371833016344c17fe3e939aaa259c3846e136d6
Antonio Santos <84925495+tonydesigner@users.noreply.github.com>
2023-07-11 17:52:47 -0300

    0

--- src/pgs/Home.jsx
+++ src/pgs/Home.jsx
@@ -1,16 +1,24 @@
 import { Login } from "./Login";
 import { Caixa } from "./Caixa"
+import { Link } from 'react-router-dom'
 export function Home() {
+  const items
+  [
+    { icon: 'user', link='/alunos', title: 'Alunos' }
+  ]
   return (
     <>
       <Login />
       <div className="home">
         <div className="dash base margin ">
-          <Caixa nome='user' title='Alunos' />
-          <Caixa nome='user' title='Alunos' />
-          <Caixa nome='user' title='Alunos' />
-          <Caixa nome='user' title='Alunos' />
-          <Caixa nome='user' title='Alunos' />
+          {
+            items.map(i =>
+              <Link to={i.lik} className='link'>
+                <Caixa nome={i.icon} title={i.title} />
+              </Link>
+            )
+          }
+
         </div>
       </div>
     </>
