116b8e8a3626bcc2be5d197a9131bd90745294ef
Antonio Santos <84925495+tonydesigner@users.noreply.github.com>
2023-07-11 16:54:35 -0300

    01

--- src/pgs/Home.jsx
+++ src/pgs/Home.jsx
@@ -6,7 +6,7 @@ export function Home() {
       <Login />
       <div className="home base">
         <div className="dash">
-         <Caixa />
+          <Caixa />
         </div>
       </div>
     </>
