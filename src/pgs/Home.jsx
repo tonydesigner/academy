9aed1b24a2348c5ef771538235f95b887186d859
Antonio Santos <84925495+tonydesigner@users.noreply.github.com>
2023-07-11 17:38:34 -0300

    7

--- src/pgs/Home.jsx
+++ src/pgs/Home.jsx
@@ -4,8 +4,12 @@ export function Home() {
   return (
     <>
       <Login />
-      <div className="home base">
+      <div className="home margin base">
         <div className="dash">
+          <Caixa nome='user' title='Alunos' />
+          <Caixa nome='user' title='Alunos' />
+          <Caixa nome='user' title='Alunos' />
+          <Caixa nome='user' title='Alunos' />
           <Caixa nome='user' title='Alunos' />
         </div>
       </div>
