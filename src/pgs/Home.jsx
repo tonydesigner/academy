891b3a5fed0792ab02074f60d018b1a207fa5820
Antonio Santos <84925495+tonydesigner@users.noreply.github.com>
2023-07-11 16:39:31 -0300

    update box

--- src/pgs/Home.jsx
+++ src/pgs/Home.jsx
@@ -6,7 +6,7 @@ export function Home() {
       <Login />
       <div className="home base">
         <div className="dash">
-          <Box name='user' title='Alunos' />
+          <Box/>
         </div>
       </div>
     </>
