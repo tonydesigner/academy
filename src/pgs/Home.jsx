af955a16f3473cd7fd0c863c7fc0d7aace9dc9a5
Antonio Santos <84925495+tonydesigner@users.noreply.github.com>
2023-07-11 18:00:26 -0300

    1

--- src/pgs/Home.jsx
+++ src/pgs/Home.jsx
@@ -1,6 +1,5 @@
 import { Login } from "./Login";
 import { Caixa } from "./Caixa"
-import { Link } from "react-router-dom"
 export function Home() {
   const items
   [
@@ -13,9 +12,9 @@ export function Home() {
         <div className="dash base margin ">
           {
             items.map(i =>
-              <Link to={i.lik} className='link'>
+             
                 <Caixa nome={i.icon} title={i.title} />
-              </Link>
+              
             )
           }
 
