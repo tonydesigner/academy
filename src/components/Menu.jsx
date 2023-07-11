010cff840ff10cda298d4ce714c70d9455968bee
Antonio Santos <84925495+tonydesigner@users.noreply.github.com>
2023-07-11 15:59:08 -0300

    update menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,29 +1,26 @@
 import { Link } from "react-router-dom"
 
-export function Menu(){
+export function Menu() {
     const menu =
-    [
-        {title: 'Financeiro', link:'/financeiro'},
-        {title: 'Alunos', link:'/alunos'},
-        {title: 'Modalidades', link:'/modalidade'},
-        {title: 'LogOut', link:'/academy'},
-    ]
-    return(
+        [
+            { title: 'Financeiro', link: '/financeiro' },
+            { title: 'Alunos', link: '/alunos' },
+            { title: 'Modalidades', link: '/modalidade' },
+            { title: 'LogOut', link: '/academy' },
+        ]
+    return (
         <>
             <div className="menu">
                 <ul>
-                {
-                    menu.map(i=>
-                        <Link to={i.link} className="link cor">
-                            <li>{i.title} </li>
-                        </Link>
-                        
-
+                    {
+                        menu.map(i =>
+                            <Link to={i.link} className="link cor">
+                                <li>{i.title} </li>
+                            </Link>
                         )
-                }
-
+                    }
                 </ul>
-                
+
             </div>
         </>
     )
