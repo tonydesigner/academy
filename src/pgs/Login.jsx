c9d71395ae54a33a6a3c58f750f41140332d6406
Antonio Santos <84925495+tonydesigner@users.noreply.github.com>
2023-07-11 16:37:50 -0300

    atual

--- src/pgs/Login.jsx
+++ src/pgs/Login.jsx
@@ -7,7 +7,7 @@ export function Login() {
     e.preventDefault();
     
     if (login == "admin" && senha == "admin") {
-      $('.login form').css('animation', 'sai 1s')
+      $('.login form').css('animation', 'sai ease')
        $('.login').hide()
     }
   };
