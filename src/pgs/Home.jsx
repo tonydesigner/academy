78311cf34d8fa3e7a71fc97c5644b2a44cd28b8b
Antonio Santos <84925495+tonydesigner@users.noreply.github.com>
2023-07-11 22:52:02 -0300

    update all

--- src/pgs/Home.jsx
+++ src/pgs/Home.jsx
@@ -3,7 +3,13 @@ import { Caixa } from "./Caixa"
 export function Home() {
   const items =
   [
-    { icon: 'user', title: 'Alunos' }
+    { icon: 'user', title: 'Alunos' },
+    { icon: 'user', title: 'Alunos' },
+    { icon: 'user', title: 'Alunos' },
+    { icon: 'user', title: 'Alunos' },
+    { icon: 'user', title: 'Alunos' },
+    { icon: 'user', title: 'Alunos' },
+    { icon: 'user', title: 'Alunos' },
   ]
   return (
     <>
