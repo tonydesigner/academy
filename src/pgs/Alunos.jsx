d2825dd4e4c9f9b00c2f205645049469148980d4
Antonio Santos <84925495+tonydesigner@users.noreply.github.com>
2023-08-19 01:06:37 -0300

    Alunos Update

--- src/pgs/Alunos.jsx
+++ src/pgs/Alunos.jsx
@@ -1,7 +1,7 @@
 export function Alunos(){
     return(
         <>
-        <div className="alunos brd base">
+        <div className="alunos base">
         <h1>Alunos 350</h1>
         </div>
             
